ed786e379dbd8291154d75e0e2017d38893b4689
isabellekitchel <isabelle.h.kitchel.23@dartmouth.edu>
2021-02-03 00:57:54 -0500

    hw3

--- personalsite/src/App.js
+++ personalsite/src/App.js
@@ -1,6 +1,10 @@
-import logo from './logo.svg';
+import Navbar from "./components/Navbar/Navbar";
 import './App.css';
 import React from 'react';
+import beach from './beach.jpeg'
+import coco from './coco.jpg'
+import me from './me.jpg'
+
 
 function App() {
   const name = "Isabelle";
@@ -25,12 +29,13 @@ function App() {
       <head>
         <title>Isabelle Kitchel</title>
       </head>
+      <Navbar />
       <body>
         <h2 className="white-text" id = "pink-background">About me:</h2>
         <button id = "pink-background" onClick={buttonFunction}>Hello!</button>
         <p className="blue-text">My name is {name}. I am a '23 at Dartmouth College. I am studying computer science
             and economics.</p>
-        <img src="./2A1D36DD-28EB-4403-979F-56BFC04582AE.jpg"/>
+        <img src={me} height={500} width={375}/>
         <div>
         <h2 className="white-text" id = "pink-background">Some things I like:</h2>
           <ul>
@@ -38,13 +43,14 @@ function App() {
             <li className="blue-text">Skiing and running</li>
             <li className="blue-text">Exploring new places</li>
           </ul>
-          <img src="personalsite/src/26D9A2E6-2AB7-4061-9AC9-FA172F1C2F1E.jpeg"/>
+          <img src={beach} height={500} width={700}/>
         </div>
         <h2 className="white-text" id = "pink-background">Family:</h2>
         <p className="blue-text">I have two brothers:.</p>
         <div>{siblingsMap}</div>
         <h2 className="white-text" id = "pink-background">My dog:</h2>
-        <img src="./IMG_6189.jpg"/>
+        <img src={coco} height={500} width={400}/>
+        <h2 className="white-text" id = "pink-background"> </h2>
         <input type="text" onChange={onChangeFunction}/>
       </body>
     </div>
---
+++ personalsite/src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'About Me',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'School',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Extracurriculars',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'For Fun',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact me',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
